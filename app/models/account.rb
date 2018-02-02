ddd95c407db87ee020e0c4428b4d0d731086caa6
kizzonia <chibuikefortune@hotmail.com>
2018-02-02 17:09:26 +0100

    image

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,7 +1,7 @@
 class Account < ApplicationRecord
+  mount_uploader :image, ImageUploader
   extend FriendlyId
   friendly_id :account_number, use: :slugged
-  mount_uploader :image, ImageUploader
 
   belongs_to :user
   validates :user, presence: true
